8ef24b1e4477ba675906acc113f6114eb679e255
mn2272 <nayeem220702@gmail.com>
2025-01-11 12:38:27 +0800

    Update main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // Import the global styles
-import App from './App';  // Import the main App component
+import '/index.css';  // Import the global styles
+import App from '/App';  // Import the main App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
